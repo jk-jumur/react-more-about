7a8fc1317d5f0cb1116f09b84b3759fb3c2f6ab2
jk-jumur <developer.jumur@gmail.com>
2026-05-20 09:43:32 +0600

    use style in react and 3 columns layout and conditional text

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,18 +1,19 @@
 import { use } from "react";
 import Country from "./Country/Country";
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesData =  use(countriesPromise);
     const countries = countriesData.countries
-      console.log(countries);
+      // console.log(countries);
     return (
         <div>
             <h3>In The Countries: {countries.length}</h3>
-              
+              <div className='countries'>
               {
                 countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
               }
-
+              </div>
         </div>
     );
 };
--- src/components/countries/Country/Country.jsx
+++ src/components/countries/Country/Country.jsx
@@ -1,18 +1,31 @@
+import './Country.css'
+
 const Country = ({country}) =>{
     //   console.log(country.name.common);
     //   console.log(country.flags.flags.png)
       //    console.log(country.population.population);
+        // console.log(country.area.area);
+
+        const handleVisited = () =>{
+             console.log('visited')
+        }
       return(
-         <div>
+         <div className="country">
                <img 
                src={country.flags.flags.png}
                alt={country.flags.flags.alt} 
-                />
+                className='country-img'/>
                <h3>Name: {country.name.common}</h3>
                <p>Population: {country.population.population}</p>
+               <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+               <button onClick={handleVisited}>Not Visited</button>
            
          </div>
       )
 }
 
 export default Country;
+
+/**
+ * 1. inline css (style object)
+ */
